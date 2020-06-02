cd3d93c9c2c59ee2eefda28d1c5f0d9a6f5f03d1
João Vitor Ferreira <j.vitor.mf@gmail.com>
2020-06-02 18:35:57 -0300

    Create DB and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  response.json(['João', 'Vitor', 'Cleiton']);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
