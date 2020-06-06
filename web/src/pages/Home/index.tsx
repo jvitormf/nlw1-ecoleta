0f938ffe7ea3ddfbe7f7a5e78a3bc912b4cd08dd
João Vitor Ferreira <j.vitor.mf@gmail.com>
2020-06-06 14:42:01 -0300

    Add items to home page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -6,6 +6,13 @@ import './styles.css'
 
 import logo from '../../assets/logo.svg';
 
+import baterias from "../../assets/items/baterias.png";
+import eletronicos from "../../assets/items/eletronicos.png";
+import lampadas from "../../assets/items/lampadas.png";
+import oleo from "../../assets/items/oleo.png";
+import papeis_papelao from "../../assets/items/papeis_papelao.png";
+import organicos from "../../assets/items/organicos.png";
+
 const Home = () => {
  return (
    <div id="page-home">
@@ -13,6 +20,15 @@ const Home = () => {
        <header>
          <img src={logo} alt="Ecoleta"/>
        </header>
+
+       <div className="items">
+          <img src={baterias} alt="Baterias" title="Baterias" />
+          <img src={eletronicos} alt="Baterias" title="Eletrônicos" />
+          <img src={lampadas} alt="Baterias" title="Lâmpadas" />
+          <img src={organicos} alt="Baterias" title="Orgânicos" />
+          <img src={oleo} alt="Baterias" title="Óleo de Cozinha" />
+          <img src={papeis_papelao} alt="Baterias" title="Papéis e papelão" />
+       </div>
 
        <main>
          <h1>Seu marketplace de coleta de resíduos.</h1>
@@ -27,7 +43,7 @@ const Home = () => {
        </main>
      </div>
    </div>
- ) 
+ )
 }
 
 export default Home;
